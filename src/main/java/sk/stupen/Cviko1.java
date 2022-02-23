adb03573807c5d98bbe486cfb501b80483c59189
Tomas <xstupen@stuba.sk>
2022-02-23 16:33:44 +0100

    funkcie array a cislo

--- src/main/java/sk/stupen/Cviko1.java
+++ src/main/java/sk/stupen/Cviko1.java
@@ -1,25 +1,33 @@
 package sk.stupen;
 
 import java.sql.SQLOutput;
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Cviko1 {
     public static void main(String[] args) {
 
-        Scanner klavesnica = new Scanner(System.in);
-        String a = "a";
-        String b = "a";
-        System.out.println("Zadaj pismeno prosim: ");
-        String c = klavesnica.nextLine();
+        int x = 5;
+        int[] y = new int[]{1, 2, 3};
 
-        System.out.println("a == a " + (a == a));
-        System.out.println("a == b " + (a == b));
-        System.out.println("a == c " + (a == c));
+        System.out.println(x);
+        testPrimitive(x);
+        System.out.println(x);
 
-        System.out.println("Objects.equals(a,a): " + Objects.equals(a,a));
-        System.out.println("Objects.equals(a,b): " + Objects.equals(a,b));
-        System.out.println("Objects.equals(a,c): " + Objects.equals(a,c));
+        System.out.println(Arrays.toString(y));
+        testArray(y);
+        System.out.println(Arrays.toString(y));
 
     }
+    public static void testPrimitive(int cislo){
+        cislo=10;
+        System.out.println(cislo);
+    }
+
+    public static void testArray(int[] array){
+        array[0]= 10;
+        System.out.println(Arrays.toString(array));
+    }
+
 }
