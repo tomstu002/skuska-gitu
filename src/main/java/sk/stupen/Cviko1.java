9e17d635327e79071556f00011744c4cf9e61a97
Tomas <xstupen@stuba.sk>
2022-02-23 16:05:32 +0100

    equals

--- src/main/java/sk/stupen/Cviko1.java
+++ src/main/java/sk/stupen/Cviko1.java
@@ -1,11 +1,25 @@
 package sk.stupen;
 
+import java.sql.SQLOutput;
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Cviko1 {
     public static void main(String[] args) {
-        //int i = 10;
-        for (int i=1;i<10;i++){
-            System.out.println("Ahoj svet");
-        }
+
+        Scanner klavesnica = new Scanner(System.in);
+        String a = "a";
+        String b = "a";
+        System.out.println("Zadaj pismeno prosim: ");
+        String c = klavesnica.nextLine();
+
+        System.out.println("a == a " + (a == a));
+        System.out.println("a == b " + (a == b));
+        System.out.println("a == c " + (a == c));
+
+        System.out.println("Objects.equals(a,a): " + Objects.equals(a,a));
+        System.out.println("Objects.equals(a,b): " + Objects.equals(a,b));
+        System.out.println("Objects.equals(a,c): " + Objects.equals(a,c));
 
     }
 }
