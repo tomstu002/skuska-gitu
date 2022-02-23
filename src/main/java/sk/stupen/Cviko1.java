fa4f0fe195fd1ed52abc6d4969ef5c1dd3ad58fe
Tomas <xstupen@stuba.sk>
2022-02-23 15:12:16 +0100

    Ahoj

--- src/main/java/sk/stupen/Cviko1.java
+++ src/main/java/sk/stupen/Cviko1.java
@@ -2,6 +2,10 @@ package sk.stupen;
 
 public class Cviko1 {
     public static void main(String[] args) {
-        System.out.println("Ahoj svet");
+        //int i = 10;
+        for (int i=1;i<10;i++){
+            System.out.println("Ahoj svet");
+        }
+
     }
 }
