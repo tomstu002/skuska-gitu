7917606f1a00fb057226af59481e89b48e395c3b
Tomas <xstupen@stuba.sk>
2022-03-02 15:09:37 +0100

    Prázdne

--- src/main/java/sk/stupen/Cviko1.java
+++ src/main/java/sk/stupen/Cviko1.java
@@ -1,33 +1,13 @@
 package sk.stupen;
 
-import java.sql.SQLOutput;
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.Scanner;
+
 
 public class Cviko1 {
     public static void main(String[] args) {
 
-        int x = 5;
-        int[] y = new int[]{1, 2, 3};
 
-        System.out.println(x);
-        testPrimitive(x);
-        System.out.println(x);
 
-        System.out.println(Arrays.toString(y));
-        testArray(y);
-        System.out.println(Arrays.toString(y));
 
-    }
-    public static void testPrimitive(int cislo){
-        cislo=10;
-        System.out.println(cislo);
-    }
-
-    public static void testArray(int[] array){
-        array[0]= 10;
-        System.out.println(Arrays.toString(array));
     }
 
 }
