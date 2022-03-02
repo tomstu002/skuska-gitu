35a97979207a7f741bcb83b1adbb116786fead61
Tomas <xstupen@stuba.sk>
2022-03-02 15:27:35 +0100

    triedy

---
+++ src/main/java/sk/stupen/Auto.java
@@ -0,0 +1,13 @@
+package sk.stupen;
+
+public class Auto {
+    int pocetKolies;
+    int pocetDveri;
+    String nazov;
+
+
+    public int jedna;
+    int dva;
+    protected int tri;
+    private int styry;
+}
--- src/main/java/sk/stupen/Cviko1.java
+++ src/main/java/sk/stupen/Cviko1.java
@@ -4,7 +4,8 @@ package sk.stupen;
 
 public class Cviko1 {
     public static void main(String[] args) {
-
+    Auto skoda = new Auto();
+    skoda.nazov = "Fabia";
 
 
 
